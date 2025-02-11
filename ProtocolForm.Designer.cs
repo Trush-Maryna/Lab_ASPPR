254643cba648d351792bb75657ca7f12fa722fd7
Trush Maryna <m.s.trush@student.khai.edu>
2025-02-11 18:32:04 +0200

    Add Protocol Form

---
+++ ProtocolForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Lab1_ASPPR
+{
+    partial class ProtocolForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxProtocol = new TextBox();
+            SuspendLayout();
+            // 
+            // textBoxProtocol
+            // 
+            textBoxProtocol.Dock = DockStyle.Fill;
+            textBoxProtocol.Location = new Point(0, 0);
+            textBoxProtocol.Multiline = true;
+            textBoxProtocol.Name = "textBoxProtocol";
+            textBoxProtocol.Size = new Size(800, 450);
+            textBoxProtocol.TabIndex = 0;
+            // 
+            // ProtocolForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBoxProtocol);
+            Name = "ProtocolForm";
+            Text = "ProtocolForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxProtocol;
+    }
+}
---
+++ ProtocolForm.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab1_ASPPR
+{
+    public partial class ProtocolForm : Form
+    {
+        public ProtocolForm(string protocol)
+        {
+            InitializeComponent();
+            textBoxProtocol.Text = protocol;
+            textBoxProtocol.ScrollBars = ScrollBars.Vertical;
+        }
+    }
+}
